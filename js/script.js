2636fdee28f31ccb81a4fa5a6d2abb53da4b72b6
Karen Reis <kreisdemenezes@gmail.com>
2022-03-20 16:36:08 -0300

    main

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 function consultarCepAjax() {
-    // Pegando o valor do campo cep
     var cep = $('#cep').val();
-    // Chamando a api passando o cep
     $.getJSON(`https://viacep.com.br/ws/${cep}/json/`, function (data, status) {
-        // Armazenando o resultado
-        var dadosCep = data
-
-        // Preenchendo os campos com o resultado
-        $('#localidade').val(dadosCep.localidade)
-        $('#bairro').val(dadosCep.bairro)
-        $('#uf').val(dadosCep.uf)
-        $('#logradouro').val(dadosCep.logradouro)
-        $('#ddd').val(dadosCep.ddd)
+  
+      try {
+  
+        if (status == 'success') {
+          var dadosCep = data
+  
+          $('#localidade').val(dadosCep.localidade)
+          $('#bairro').val(dadosCep.bairro)
+          $('#uf').val(dadosCep.uf)
+          $('#logradouro').val(dadosCep.logradouro)
+          $('#ddd').val(dadosCep.ddd)
+        }
+      } catch (error) {
+        console.log(error)
+      }
     });
-}
+  }
